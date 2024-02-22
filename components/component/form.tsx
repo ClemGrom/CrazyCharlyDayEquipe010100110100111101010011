02fc0380096f187e97194415d49ca696685b5b9b
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-02-22 18:32:51 +0100

    feat: add form component

--- components/component/form.tsx
+++ components/component/form.tsx
@@ -1,11 +1,12 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { login, signup } from "@/app/signin/action";
 
-export function Form({ onClick }: { onClick: () => void }) {
+export function Form() {
   return (
     <div className="flex items-center min-h-screen px-4">
-      <div className="w-full max-w-sm mx-auto space-y-4">
+      <form className="w-full max-w-sm mx-auto space-y-4">
         <h2 className="text-2xl font-bold">Formulaire d'inscription</h2>
         <div className="space-y-2">
           <div className="space-y-2">
@@ -22,14 +23,22 @@ export function Form({ onClick }: { onClick: () => void }) {
               id="email"
               placeholder="Entrez votre adresse mail"
               required
+              name="email"
               type="email"
             />
           </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Mot de passe</Label>
+            <Input id="password" placeholder="Entrez votre mot de passe" required type="password" />
+          </div>
         </div>
-        <Button className="w-full" onClick={onClick}>
-          Soumettre
+        <Button className="w-full" formAction={login}>
+          Se connecter
         </Button>
-      </div>
+        <Button className="w-full" formAction={signup}>
+          S'inscrire
+        </Button>
+      </form>
     </div>
   );
 }
