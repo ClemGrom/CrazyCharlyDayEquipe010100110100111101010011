6b217620ebe3f0125a1f6b7a4f1a8e66a777fbb8
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-22 19:32:10 +0100

    feat(ateliers): remove button create atelier, only admin

--- app/ateliers/page.tsx
+++ app/ateliers/page.tsx
@@ -19,11 +19,6 @@ export default async function Page() {
     return (<div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900">
             <main
                 className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40">
-                <Link href="/create">
-                <div className="max-w-6xl w-full mx-auto flex items-center gap-4">
-                    <Button>Créer un atelier</Button>
-                </div>
-                </Link>
                 <div className="max-w-6xl w-full mx-auto">
                     <Suspense fallback={<AtelierList ateliers={[]} isLoading={true}/>}>
                         <AtelierList ateliers={ateliers || []} isLoading={false}/>
