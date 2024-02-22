5ce16d76d15139a11a916c80c72e627183cfe3fe
Victor.L <victor.lath9@etu.univ-lorraine.fr>
2024-02-22 11:29:10 +0100

    score

---
+++ app/optimisation/score.ts
@@ -0,0 +1,13 @@
+// Script d'automatisation du système de score 
+
+const checkLimitAtelier = () => { };
+
+const checkDouble = () => { };
+
+const gainByPreference = () => { };
+
+const degression = () => { };
+
+const allAreServe = () => { };
+
+const equity = () => { };
