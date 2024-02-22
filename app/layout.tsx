c860d6b200729e8f52f6880cbfccb1811e354c17
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-22 15:03:22 +0100

    style(layout): add inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,20 @@ import "./globals.css";
 import NavbarPartial from "@/components/navbar";
 import FooterPartial from "@/components/footer";
 
+import { Inter } from 'next/font/google'
+
 export const metadata: Metadata = {
     title: "CookingBook", description: "Crazy Charly Day 2024",
 };
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
                                        children,
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return (<html lang="fr">
+    return (<html lang="fr" className={inter.className}>
     <body>
     <NavbarPartial/>
     <div className="min-h-screen">
