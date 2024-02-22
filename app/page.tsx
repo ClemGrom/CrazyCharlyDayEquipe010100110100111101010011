ed965120455de65b42ba33d9ded4f96d583e486c
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-02-22 11:52:56 +0100

    feat: add beginning of user form page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-        <h1>CCD 2024</h1>
+      <h1>CCD 2024</h1>
+      <Link href="/form">Formulation des voeux</Link>
     </div>
   );
 }
