a503a9f21c74d4491b100745d1bfe2d76bffc18f
Yann Mijatovic <yannmijatovicp@gmail.com>
2024-02-22 18:55:10 +0100

    algo

--- app/optimisation/algo.ts
+++ app/optimisation/algo.ts
@@ -1,11 +1,5 @@
 import { Atelier, Participant } from "./models";
 
-
-interface algoParams {
-    listeAteliers: Atelier[];
-    listeParticipants: Participant[];
-}
-
 interface ParticipantAlgo {
     participant: Participant;
     score: number;
@@ -13,35 +7,79 @@ interface ParticipantAlgo {
     listeAteliers: Atelier[];
 }
 
+interface RetourAlgo {
+    points: number;
+    participants: ParticipantAlgo[];
+}
 
-const algo = (algoParams: algoParams) => {
+interface colonneRegle3 {
+    rang: number;
+    points: number;
+}
 
-    const tempParticipants: Participant[] = [];
+const tableauRegle3: colonneRegle3[] = [
+    { rang: 1, points: 10 },
+    { rang: 2, points: 8 },
+    { rang: 3, points: 6 },
+    { rang: 4, points: 4 },
+    { rang: 5, points: 2 },
+    { rang: 6, points: 1 },
+]
+
+function prepaAlgo(listeAteliers: Atelier[], listeParticipants: ParticipantAlgo[], nbDemandes: number, nbPlacesAteliers: number): RetourAlgo {
+
+    const retour: RetourAlgo = { points: 0, participants: [] };
+
+    if (nbDemandes != 1 && nbPlacesAteliers != 1) {
+        listeParticipants.forEach((p) => {
+            if (p.nbAtelier < p.participant.nbDemande) {
+                const voeux = [p.participant.ordre1, p.participant.ordre2, p.participant.ordre3, p.participant.ordre4, p.participant.ordre5, p.participant.ordre6];
+                for (let i = 0; i < voeux.length; i++) {
+                    const voeu = voeux[i];
+                    for (let j = 0; j < listeAteliers.length; j++) {
+                        const atelier = listeAteliers[j];
+                        if (atelier.theme.match(voeu) && atelier.nb > 0 && atelier.listeIdParticipants.includes(p.participant.id)) {
+                            atelier.listeIdParticipants.push(p.participant.id);
+                            atelier.nb -= 1;
+                            nbDemandes -= 1;
+                            nbPlacesAteliers -= 1;
+                            p.nbAtelier += 1;
+                            // Règle  3 +  4
+                            var rang = voeux.indexOf(voeu) + p.nbAtelier;
+                            if (rang > 5) rang = 5;
+                            p.score = tableauRegle3[rang].points;
+                            break; // Breaks out of the inner loop
+                        }
+                    }
+                }
+            }
+        })
+    } else {
+
+    }
+}
+
+
+function algo(listeAteliers: Atelier[], listeParticipants: Participant[]): number {
+
     const listeParticipantsAlgo: ParticipantAlgo[] = [];
-    tempParticipants.forEach(participant => {
+    var demandes: number;
+    var nbPlaces: number;
+
+    listeParticipants.forEach(participant => {
         let participantAlgo = {} as ParticipantAlgo;
         participantAlgo.participant = participant;
         participantAlgo.score = 0;
         participantAlgo.nbAtelier = 0;
         participantAlgo.listeAteliers = [];
         listeParticipantsAlgo.push(participantAlgo);
+        demandes += participant.nbDemande;
     });
+    listeAteliers.forEach(atelier => {
+        nbPlaces += atelier.nb;
+    })
 
-
-    // {idParticipant : score}
-    let atelierDispo = true;
-    let demandeFournis = false;
-    while (atelierDispo && !demandeFournis) {
-        algoParams.listeAteliers.forEach(atelier => {
-            if (atelier.nb < atelier.listeIdParticipants.length) {
-                atelierDispo = false;
-            }
-        });
-        //Liste des participants avec ces choix
-        let listeParticipantsChoix = ;
-        
-        listeParticipantsAlgo.forEach(participantAlgo => {
-
-        });
-    }
+    return prepaAlgo(listeAteliers, listeParticipantsAlgo, demandes, nbPlaces).points;
 }
+
+export { algo }
