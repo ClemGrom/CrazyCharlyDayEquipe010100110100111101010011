4a77a24a53eda0f455752fdfb672c304327a578b
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-22 18:33:40 +0100

    feat(hero): skew effect with cards aggregate

--- app/ateliers/page.tsx
+++ app/ateliers/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
             <main
                 className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40">
                 <div className="max-w-6xl w-full mx-auto flex items-center gap-4">
-                    <Button className="bg-blue-500 hover:bg-blue-600 text-white">Créer un atelier</Button>
+                    <Button>Créer un atelier</Button>
                 </div>
                 <div className="max-w-6xl w-full mx-auto">
                     <Suspense fallback={<AtelierList ateliers={[]} isLoading={true}/>}>
